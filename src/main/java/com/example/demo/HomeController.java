338e96aebdfee130f0441bcf3bdc5ad71e25d3dc
Shannon Foster <shannon.fos3@outlook.com>
2018-03-26 15:18:44 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomeController {
+
+        @RequestMapping("/loadForm")
+        public String loadFormPage(){
+                //System.out.println("in the loadForm method");
+                return "formtemplate";
+        }
+        @RequestMapping("/processform")
+        public String loadFromPage(@RequestParam("login") String loginvalue, Model model) {
+
+                model.addAttribute( "loginvalue", loginvalue);
+                model.addAttribute( "shannon", "My name is shannon");
+                return "confirm";
+        }
+}
